fb8a5a540559ba205ed1a8b1471485c5cddea3cb
Karin Keijzer <karin.keijzer@capgemini.com>
2019-02-28 21:40:01 +0100

    First working login service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,25 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import { map } from 'rxjs/operators';
-// import decode from 'jwt-decode';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
-@Injectable()
-export default class AuthService {
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
 
   constructor(private http: HttpClient) {
-
   }
 
-  login(username:string, password:string ) {
-    console.log('lgoin user', username);
-    return this.http.post<any>(`/user/login`, { username, password })
+  static getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  static logout() {
+    localStorage.removeItem("id_token");
+  }
+
+  login(username: string, password: string) {
+    return this.http.post<any>(`/user/login`, {username, password})
       .pipe(map(user => {
         // login successful if there's a jwt token in the response
         if (user && user.token) {
@@ -24,24 +31,16 @@ export default class AuthService {
       }));
   }
 
-  private setSession(token) {
-    localStorage.setItem('id_token', token);
-  }
-
-  static getToken() {
-    return localStorage.getItem('id_token');
-  }
-
-  static logout() {
-    localStorage.removeItem("id_token");
-  }
-
   public isLoggedIn() {
     return false;
   }
 
   isLoggedOut() {
     return !this.isLoggedIn();
+  }
+
+  private setSession(token) {
+    localStorage.setItem('id_token', token);
   }
 
 }
