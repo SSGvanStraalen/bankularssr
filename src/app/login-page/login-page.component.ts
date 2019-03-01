ad4b7c4fabce259e7ac1df0ab14f5c1d1d3cd670
Sven van Straalen <ssgvanstraalen@gmail.com>
2019-03-01 14:11:22 +0100

    added routing when logged in

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../services/auth.service';
+import {first} from "rxjs/operators";
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,8 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  public username: string;
+  public password: string;
+  public message: string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
+
+  checkPass() {
+    if (this.username === this.password) {
+      console.log('You shall pass');
+      this.authService.login(this.username, this.password)
+        .pipe(first())
+        .subscribe(
+          data => {
+            this.router.navigateByUrl('/overview');    
+            console.log('data',data);
+          },
+          error => {
+            console.log('error',error);
+          });
+    } else {
+      this.message = 'Nope you shall not pass'
+      console.log('you shall not pass')
+    }
+  }
 
   ngOnInit() {
   }
