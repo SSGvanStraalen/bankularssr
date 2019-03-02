6ece8dfdf1fd5acf7ce8abbdd3a453f25eb511db
Sven van Straalen <ssgvanstraalen@gmail.com>
2019-03-02 22:39:47 +0100

    added meta data

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../services/auth.service';
 import {first} from "rxjs/operators";
 import {Router} from '@angular/router';
-
+import { Meta } from '@angular/platform-browser';
+import { Title }     from '@angular/platform-browser';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -13,7 +14,34 @@ export class LoginPageComponent implements OnInit {
   public password: string;
   public message: string;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private meta: Meta,
+    private titleService: Title
+    ) { 
+
+    this.titleService.setTitle('Bankular login');
+    this.meta.addTags([
+      { name: 'description', content: 'There is only one bank where kitty money is real and thats this one and here you can login' },
+      { name: 'author', content: 'mr Bankular' },
+      { name: 'keywords', content: 'Angular, bank, kitty, login' },
+      { name: 'og:title', content: 'Bankular login'},
+      { name: 'og:description', content: 'Login page bankular where kitty money exists'},
+      { name: 'og:url', content: 'http://bankularx-env.wzmxwbagad.eu-west-1.elasticbeanstalk.com/'},
+      { name: 'og:image', content: 'http://bankularx-env.wzmxwbagad.eu-west-1.elasticbeanstalk.com/assets/KittyCard-Top.png'},
+
+
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@SvenvanStraalen' },
+      { name: 'twitter:creator', content: '@twitter-username'},
+      { name: 'twitter:title', content: 'Bankular login'},
+      { name: 'twitter:description', content: 'Bankular login page'},
+      { name: 'twitter:image', content: 'http://bankularx-env.wzmxwbagad.eu-west-1.elasticbeanstalk.com/assets/KittyCard-Top.png'},
+
+    ])
+
+  }
 
   checkPass() {
     if (this.username === this.password) {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  //apiUrl: 'http://bankularx-env.wzmxwbagad.eu-west-1.elasticbeanstalk.com'
   apiUrl: 'http://localhost:4000'
 };
