8ac62abc3187280f5deb000d015541e2bb12e7e9
Karin Keijzer <karin.keijzer@capgemini.com>
2019-03-03 21:59:39 +0100

    Transfer moneyz

--- routes/common/accounts.js
+++ routes/common/accounts.js
@@ -14,21 +14,24 @@ function getAccounts(username) {
 }
 
 function transfer(username, fromAcc, toAcc, amount) {
-  if (getAccounts(username).find(acc => {
+  let from = getAccounts(username).find(acc => {
     return fromAcc == acc.accnr;
-  })) {
+  });
+  if (from) {
+    // all accounts
     let as = [].concat.apply([], Object.values(accounts));
-    let from = as.find(a=>{return a.accnr == fromAcc});
-    let to = as.find(a=>{return a.accnr == toAcc});
-    if (from && to) {
-      from.balance = from.balance-amount;
-      to.balance = to.balance+amount;
-      return {success:true, message:'Transfer complete.'}
+    let to = as.find(a => {
+      return a.accnr == toAcc
+    });
+    if (to) {
+      from.balance = from.balance - amount;
+      to.balance = to.balance + amount;
+      return {success: true, message: 'Transfer complete.'}
     } else {
-      return {success:false, message:'Account not found.'}
+      return {success: false, message: 'Beneficiary account not found.'}
     }
   } else {
-    return {success:false, message:'Not your account.'}
+    return {success: false, message: 'Not your own account.'}
   }
 }
 
